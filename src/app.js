64a1fa5ab985573396b22a07300bf1a25551488f
Isaac Graciano <isaacgraciano123@gmail.com>
2022-09-15 20:14:33 -0300

    Creat and Get Products

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRouter from './routers/authRouter.js'
+import authRouter from './routers/authRouter.js';
+import productRouter from "./routers/productRouter.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(authRouter);
+app.use(productRouter);
 
 const PORT = process.env.PORT
 app.listen(PORT || 5000, () => console.log(`server listen on port ${PORT}`))
