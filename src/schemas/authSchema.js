2ee10b9ff6ef65d26804632e32caa0afe84a7be3
Edson0012 <Edsonewertonrs1@gmail.com>
2022-09-16 06:26:30 -0300

    adicionando função signIn-login

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -8,5 +8,5 @@ export const signUpSchema = joi.object({
 
 export const signInSchema = joi.object({
     email: joi.string().email().required(),
-    password: joi.string().required().min(6),
+    password: joi.string().min(6).required(),
 });
